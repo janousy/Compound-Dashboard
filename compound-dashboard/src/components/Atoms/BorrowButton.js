5617615fd6ec19467da110b90bf1ee762552050d
janousy <janosch.baltensperger@gmail.com>
2021-11-21 15:42:59 +0100

    implement borrow interface

--- compound-dashboard/src/components/Atoms/BorrowButton.js
+++ compound-dashboard/src/components/Atoms/BorrowButton.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Row from "react-bootstrap/Row";
 import {Button, Dropdown, DropdownButton, FormControl} from "react-bootstrap";
 import '../../Styles/Molecules/Borrow.css';
+import {borrowErc20} from "../../scripts/borrow-erc20-via-solidity";
+
 
 class BorrowButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { currency: '' }
+        this.state = { currency: '', collateral: '', borrowAmount: 0 }
+    }
+    componentDidMount() {
     }
 
-    componentDidMount() {
+    handleBorrowCurrency = (event) => {
+        console.log(event)
+        if(event === 'UZHETH') {
+            this.setState({currency: event, collateral: 'ERC20'})
+        } else {
+            this.setState({currency: event, collateral: 'UZHETH'})
+        }
+    }
+
+    onClickBorrow = () => {
+        console.log(this.state.borrowAmount)
     }
 
     render() {
@@ -17,16 +31,19 @@ class BorrowButton extends React.Component {
         <Row className="ms-3">
             <div className="d-flex flex-column BoxContainer">
                 <div className="SubTitle mt-2">Borrow</div>
-                <FormControl className="Form mt-2 text-center" placeholder="Amount to Borrow"/>
-                <DropdownButton className="mt-2" title="Select Currency">
-                    <Dropdown.Item href="#/action-1">UZHETH</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">UZHDAI</Dropdown.Item>
+                <FormControl onChange={event => {this.setState({borrowAmount: event.target.value})}}
+                             className="Form mt-2 text-center"
+                             placeholder="Amount to Borrow"
+                             value={this.state.borrowAmount}/>
+                <DropdownButton onSelect={this.handleBorrowCurrency} className="mt-2" title={"Currency: " + this.state.currency}>
+                    <Dropdown.Item eventKey="UZHETH" value='UZHETH' href="#/action-1">UZHETH</Dropdown.Item>
+                    <Dropdown.Item eventKey="ERC20" value='ERC20' href="#/action-2">ERC20</Dropdown.Item>
                 </DropdownButton>
-                <DropdownButton className="mt-2" title="Select Collateral">
-                    <Dropdown.Item href="#/action-1">UZHETH</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">UZHDAI</Dropdown.Item>
+                <DropdownButton className="mt-2" disabled title={"Collateral: " + this.state.collateral}>
+                    <Dropdown.Item eventKey="UZHETH">UZHETH</Dropdown.Item>
+                    <Dropdown.Item eventKey="ERC20">ERC20</Dropdown.Item>
                 </DropdownButton>
-                <Button className="Button mb-3 mt-2" variant="primary">Borrow</Button>
+                <Button onClick={this.onClickBorrow} className="Button mb-3 mt-2" variant="primary">Borrow</Button>
             </div>
         </Row>
         )
---
+++ compound-dashboard/src/scripts/borrow-erc20-via-solidity.js
@@ -0,0 +1,126 @@
+// Example to borrow DAI (or any ERC20 token) using ETH as collateral
+// from a Solidity smart contract
+const Web3 = require('web3');
+const web3 = new Web3('http://127.0.0.1:8545');
+const {
+  cEthAbi,
+  cErcAbi,
+  erc20Abi,
+} = require('./contracts/contracts.json');
+
+// Your Ethereum wallet private key
+const privateKey = 'b8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329';
+
+// Add your Ethereum wallet to the Web3 object
+web3.eth.accounts.wallet.add('0x' + privateKey);
+const myWalletAddress = web3.eth.accounts.wallet[0].address;
+
+// Mainnet Contract for cETH (the collateral-supply process is different for cERC20 tokens)
+const cEthAddress = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5';
+const cEth = new web3.eth.Contract(cEthAbi, cEthAddress);
+
+// Mainnet Contract for the Comptroller & Open Price Feed
+const comptrollerAddress = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b';
+const priceFeedAddress = '0x6d2299c48a8dd07a872fdd0f8233924872ad1071';
+
+// Mainnet address of underlying token (like DAI or USDC)
+const underlyingAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // Dai
+const underlying = new web3.eth.Contract(erc20Abi, underlyingAddress);
+
+// Mainnet address for a cToken (like cDai, https://compound.finance/docs#networks)
+const cTokenAddress = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643'; // cDai
+const cToken = new web3.eth.Contract(cErcAbi, cTokenAddress);
+const assetName = 'DAI'; // for the log output lines
+const underlyingDecimals = 18; // Number of decimals defined in this ERC20 token's contract
+
+// MyContract
+const borrowContractAbi = require('./contracts/CompoundBorrow.json').abi;
+const borrowContractAddress = '0xEcA3eDfD09435C2C7D2583124ca9a44f82aF1e8b';
+const borrowContract = new web3.eth.Contract(borrowContractAbi, borrowContractAddress);
+
+const logBalances = () => {
+  return new Promise(async (resolve, reject) => {
+    let myWalletEthBalance = +web3.utils.fromWei(await web3.eth.getBalance(myWalletAddress));
+    let myContractEthBalance = +web3.utils.fromWei(await web3.eth.getBalance(borrowContractAddress));
+    let myContractCEthBalance = await cEth.methods.balanceOf(borrowContractAddress).call() / 1e8;
+    let myContractUnderlyingBalance = +await underlying.methods.balanceOf(borrowContractAddress).call() / Math.pow(10, underlyingDecimals);
+
+    console.log("My Wallet's   ETH Balance:", myWalletEthBalance);
+    console.log("MyContract's  ETH Balance:", myContractEthBalance);
+    console.log("MyContract's cETH Balance:", myContractCEthBalance);
+    console.log(`MyContract's  ${assetName} Balance:`, myContractUnderlyingBalance);
+
+    resolve();
+  });
+};
+
+export async function borrowErc20(numUnderlyingToBorrow = 10, ethToSupplyAsCollateral = 1) {
+  console.log('attempting borrow')
+
+  const Web3 = require('web3');
+  const web3 = new Web3('http://127.0.0.1:8545');
+  const {
+    cEthAbi,
+    cErcAbi,
+    erc20Abi,
+  } = require('./contracts/contracts.json');
+
+  // Your Ethereum wallet private key
+  const privateKey = 'b8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329';
+
+// Add your Ethereum wallet to the Web3 object
+  web3.eth.accounts.wallet.add('0x' + privateKey);
+  const myWalletAddress = web3.eth.accounts.wallet[0].address;
+
+  const contractIsDeployed = (await web3.eth.getCode(borrowContractAddress)) !== '0x';
+
+  if (!contractIsDeployed) {
+    throw Error('Compound BorrowContract is not deployed! Deploy it by running the deploy script.');
+  }
+
+  await logBalances();
+
+  console.log(`\nCalling CompoundBorrow with ${ethToSupplyAsCollateral} ETH for collateral...\n`);
+
+  let result = await borrowContract.methods.borrowErc20Example(
+      cEthAddress,
+      comptrollerAddress,
+      priceFeedAddress,
+      cTokenAddress,
+      underlyingDecimals,
+      numUnderlyingToBorrow
+    ).send({
+    from: myWalletAddress,
+    gasLimit: web3.utils.toHex(5000000),
+    gasPrice: web3.utils.toHex(20000000000), // use ethgasstation.info (mainnet only)
+    value: (ethToSupplyAsCollateral * 1e18).toString()
+  });
+
+  // See the solidity functions logs from "MyLog" event
+  // console.log(result.events.MyLog);
+
+  await logBalances();
+
+/*  console.log(`\nNow repaying the borrow...\n`);
+  const underlyingToRepayBorrow = 10;
+  result = await borrowContract.methods.myErc20RepayBorrow(
+      underlyingAddress,
+      cTokenAddress,
+      (underlyingToRepayBorrow * Math.pow(10, underlyingDecimals)).toString()
+    ).send({
+    from: myWalletAddress,
+    gasLimit: web3.utils.toHex(5000000),
+    gasPrice: web3.utils.toHex(20000000000), // use ethgasstation.info (mainnet only)
+  });*/
+
+  await logBalances();
+};
+
+borrowErc20().catch(async (err) => {
+  console.error('ERROR:', err);
+
+  // Create "events" and "emit" them in your Solidity code.
+  // Current contract does not have any.
+  let logs = await borrowContract.getPastEvents('allEvents');
+  console.log('Logs: ', logs);
+});
